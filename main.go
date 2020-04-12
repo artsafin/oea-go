bb329c456f1d9331b58f0f79dcb71ee99e8fc1d2
Artur Safin <artur.safin@forextime.com>
2020-04-13 00:35:28 +0300

    Start Auth middleware

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 //go:generate go-bindata -o "common/bindata.go" -pkg "common" resources/ resources/partials/
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
-	"net/http"
 	"oea-go/common"
 	"oea-go/employee"
 	"oea-go/office"
@@ -44,22 +42,18 @@ func main() {
 	*/
 	officeHandler := office.NewHandler(&cfg)
 	employeesHandler := employee.NewHandler(&cfg)
-	listenAndServe(func(router *mux.Router) {
+	listenAndServe(func(router *webRouter) {
 		GET := router.Methods("GET").Subrouter()
 
-		globals := &templateGlobals{GET}
-
 		GET.HandleFunc("/office/{invoice:.+}/invoice", officeHandler.DownloadInvoice).Name("OfficeDownloadInvoice")
-		GET.HandleFunc("/office/{invoice:.+}", partial([]string{"office_invoice_data"}, globals, officeHandler.ShowInvoiceData)).Name("OfficeShowInvoiceData")
-		GET.HandleFunc("/office", partial([]string{"office"}, globals, officeHandler.Home)).Name("OfficeHome")
+		GET.HandleFunc("/office/{invoice:.+}", router.partial(officeHandler.ShowInvoiceData, "office_invoice_data")).Name("OfficeShowInvoiceData")
+		GET.HandleFunc("/office", router.partial(officeHandler.Home, "office")).Name("OfficeHome")
 
-		GET.HandleFunc("/employees", partial([]string{"employees"}, globals, employeesHandler.Home)).Name("EmployeesHome")
-		GET.HandleFunc("/employee/{month}", partial([]string{"employees", "employees_month"}, globals, employeesHandler.Month)).Name("EmployeesMonth")
+		GET.HandleFunc("/employees", router.partial(employeesHandler.Home, "employees")).Name("EmployeesHome")
+		GET.HandleFunc("/employee/{month}", router.partial(employeesHandler.Month, "employees", "employees_month")).Name("EmployeesMonth")
 		GET.HandleFunc("/employee/{month}/invoices", employeesHandler.DownloadAllInvoices).Name("EmployeesDownloadAllInvoices")
 		GET.HandleFunc("/employee/{month}/{employee}/invoice", employeesHandler.DownloadInvoice).Name("EmployeesDownloadInvoice")
 
-		GET.HandleFunc("/", partial([]string{"index"}, globals, nilTemplateData))
-
-		router.NotFoundHandler = http.HandlerFunc(partial([]string{"404"}, globals, nilTemplateData))
+		GET.HandleFunc("/", router.partial(nilTemplateData, "index"))
 	})
 }
--- web.go
+++ web.go
@@ -11,8 +11,14 @@ import (
 	"time"
 )
 
-func layout(router *mux.Router) *template.Template {
-	funcMap := template.FuncMap{
+const AuthCookieName = "a"
+
+type webRouter struct {
+	*mux.Router
+}
+
+func (router *webRouter) createFuncMap() template.FuncMap {
+	return template.FuncMap{
 		"link": func(routeName string, args ...string) string {
 			var route *mux.Route
 			var u *url.URL
@@ -26,16 +32,22 @@ func layout(router *mux.Router) *template.Template {
 			return u.String()
 		},
 	}
+}
 
-	return template.Must(template.New("layout").Funcs(funcMap).Parse(string(common.MustAsset("resources/layout.go.html"))))
+func (router *webRouter) parseLayout() *template.Template {
+	return template.Must(
+		template.
+			New("layout").
+			Funcs(router.createFuncMap()).
+			Parse(string(common.MustAsset("resources/layout.go.html"))))
 }
 
 type templateGlobals struct {
 	Router *mux.Router
 }
 
-func partial(names []string, globals *templateGlobals, templateDataFn func(map[string]string, *http.Request) interface{}) func(http.ResponseWriter, *http.Request) {
-	tpl := layout(globals.Router)
+func (router *webRouter) partial(templateDataFn func(map[string]string, *http.Request) interface{}, names ...string) func(http.ResponseWriter, *http.Request) {
+	tpl := router.parseLayout()
 	for _, name := range names {
 		tpl = template.Must(tpl.Parse(string(common.MustAsset(fmt.Sprintf("resources/partials/%s.go.html", name)))))
 	}
@@ -53,24 +65,23 @@ func partial(names []string, globals *templateGlobals, templateDataFn func(map[s
 	}
 }
 
-func listenAndServe(routerConfigurer func(*mux.Router)) {
-	router := mux.NewRouter()
-	router.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Header().Add("Content-Type", "image/png")
-		writer.WriteHeader(http.StatusOK)
-		writer.Write(common.MustAsset("resources/icon.png"))
-	})
+func listenAndServe(routerConfigurer func(*webRouter)) {
+	router := &webRouter{mux.NewRouter()}
+	auth := &AuthMiddleware{
+		router: router,
+	}
+	router.Use(faviconMiddleware)
+	router.Use(loggerMiddleware)
+	router.Use(auth.Middleware)
+	router.NotFoundHandler = http.HandlerFunc(router.partial(nilTemplateData, "404"))
+
 	routerConfigurer(router)
 
 	log.Println("Server started")
 
 	server := &http.Server{
-		Addr: ":8080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Println("Request:", r.Method, r.RequestURI, r.RemoteAddr, r.UserAgent())
-
-			router.ServeHTTP(w, r)
-		}),
+		Addr:         ":8080",
+		Handler:      router,
 		WriteTimeout: 60 * time.Second,
 		ReadTimeout:  60 * time.Second,
 	}
@@ -81,4 +92,44 @@ func listenAndServe(routerConfigurer func(*mux.Router)) {
 
 func nilTemplateData(vars map[string]string, req *http.Request) interface{} {
 	return nil
+}
+
+type AuthMiddleware struct {
+	router *webRouter
+}
+
+func (auth *AuthMiddleware) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authCookie, cookieErr := r.Cookie(AuthCookieName)
+		if cookieErr != nil {
+			log.Println("Forbidden:", r.Method, r.RequestURI)
+			auth.router.partial(nilTemplateData, "403")(w, r)
+			return
+		}
+
+		log.Printf("Auth cookie found: %v\n", authCookie)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func faviconMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if request.RequestURI == "/favicon.ico" {
+			writer.Header().Add("Content-Type", "image/png")
+			writer.WriteHeader(http.StatusOK)
+			writer.Write(common.MustAsset("resources/icon.png"))
+			return
+		}
+
+		next.ServeHTTP(writer, request)
+	})
+}
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, r *http.Request) {
+		log.Println("Request:", r.Method, r.RequestURI, r.RemoteAddr, r.UserAgent())
+
+		next.ServeHTTP(writer, r)
+	})
 }
